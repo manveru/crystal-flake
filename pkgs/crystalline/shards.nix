c4d08486cfad957b1e1c74f23fdfca84e0532ff5
Peter Hoeg <peter@hoeg.com>
2023-01-13 15:11:53 +0800

    crystal: 1.6.2 -> 1.7.0

--- pkgs/crystalline/shards.nix
+++ pkgs/crystalline/shards.nix
@@ -1,26 +1,27 @@
 {
   bisect = {
-    owner = "spider-gazelle";
-    repo = "bisect";
+    url = "https://github.com/spider-gazelle/bisect.git";
     rev = "v1.2.1";
     sha256 = "1ddz7fag1l65m6g0vw6xa96yv00rdwjj2z69k26rvyz37qk9ccqg";
   };
+  lsp = {
+    url = "https://github.com/elbywan/crystal-lsp.git";
+    rev = "v0.1.2";
+    sha256 = "0knw8xaq3ssyb34w77a390j79m4w6bks5hlwr8m8fci2gq9a0r6z";
+  };
   priority-queue = {
-    owner = "spider-gazelle";
-    repo = "priority-queue";
+    url = "https://github.com/spider-gazelle/priority-queue.git";
     rev = "v1.0.1";
     sha256 = "1rkppd8win4yalxcvsxikqcq6sw0npdqjajqbj57m78bzlxpyjv6";
   };
   sentry = {
-    owner = "samueleaton";
-    repo = "sentry";
-    rev = "cd86128f94567594da9ddff78cc428c697754d88";
-    sha256 = "0sralc92wckmbf825mw8gsds8515qccaip8knkilzi8naap7yn3a";
+    url = "https://github.com/samueleaton/sentry.git";
+    rev = "e448ce83486f99ef016c311e10ec0cac805cded3";
+    sha256 = "13yp7805xpd605jpfpb3srqb0psy25w7n6x9mpkcyvzhqmpnpfyq";
   };
   version_from_shard = {
-    owner = "hugopl";
-    repo = "version_from_shard";
-    rev = "v1.2.4";
-    sha256 = "0y684h29ca4fld5mz9jfvvb3kla87w6daskw4vzaic0qxxj869g8";
+    url = "https://github.com/hugopl/version_from_shard.git";
+    rev = "v1.2.5";
+    sha256 = "0xizj0q4rd541rwjbx04cjifc2gfx4l5v6q2y7gmd0ndjmkgb8ik";
   };
 }
