9986d0f61555ad40c918507963d1f6baff24b673
Michael Fellinger <michael.fellinger@iohk.io>
2023-10-05 13:50:23 +0200

    crystal 1.9.2

--- flake.nix
+++ flake.nix
@@ -6,37 +6,37 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
 
     crystal-src = {
-      url = "github:crystal-lang/crystal/1.8.1";
+      url = "github:crystal-lang/crystal/1.9.2";
       flake = false;
     };
 
     crystal-x86_64-darwin = {
-      url = "https://github.com/crystal-lang/crystal/releases/download/1.8.1/crystal-1.8.1-1-darwin-universal.tar.gz";
+      url = "https://github.com/crystal-lang/crystal/releases/download/1.9.2/crystal-1.9.2-1-darwin-universal.tar.gz";
       flake = false;
     };
 
     crystal-x86_64-linux = {
-      url = "https://github.com/crystal-lang/crystal/releases/download/1.8.1/crystal-1.8.1-1-linux-x86_64.tar.gz";
+      url = "https://github.com/crystal-lang/crystal/releases/download/1.9.2/crystal-1.9.2-1-linux-x86_64.tar.gz";
       flake = false;
     };
 
     crystal-aarch64-darwin = {
-      url = "https://github.com/crystal-lang/crystal/releases/download/1.8.1/crystal-1.8.1-1-darwin-universal.tar.gz";
+      url = "https://github.com/crystal-lang/crystal/releases/download/1.9.2/crystal-1.9.2-1-darwin-universal.tar.gz";
       flake = false;
     };
 
     bdwgc-src = {
-      url = "github:ivmai/bdwgc/v8.2.2";
+      url = "github:ivmai/bdwgc/v8.2.4";
       flake = false;
     };
 
     crystalline-src = {
-      url = "github:elbywan/crystalline/v0.9.0";
+      url = "github:elbywan/crystalline/v0.10.0";
       flake = false;
     };
 
     ameba-src = {
-      url = "github:crystal-ameba/ameba/v1.4.3";
+      url = "github:crystal-ameba/ameba/v1.5.0";
       flake = false;
     };
   };
@@ -53,10 +53,10 @@
         ...
       }: {
         overlayAttrs = let
-          crystalVersion = "1.8.1";
-          crystallineVersion = "0.9.0";
-          bdwgcVersion = "8.2.2";
-          amebaVersion = "1.4.3";
+          crystalVersion = "1.9.2";
+          crystallineVersion = "0.10.0";
+          bdwgcVersion = "8.2.4";
+          amebaVersion = "1.5.0";
           llvmPackages = pkgs.llvmPackages_11;
         in {
           bdwgc = pkgs.callPackage ./pkgs/bdwgc {
